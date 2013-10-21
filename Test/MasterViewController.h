e99c851a6a39245eb0d209e561d87592cf438409
Paril Shah <parilshah30@gmail.com>
2013-10-21 13:01:04 +0530

    Initial Commit

---
+++ Test/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  Test
+//
+//  Created by Paril Shah on 21/10/13.
+//  Copyright (c) 2013 Paril Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
---
+++ Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Paril Shah on 21/10/13.
+//  Copyright (c) 2013 Paril Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
